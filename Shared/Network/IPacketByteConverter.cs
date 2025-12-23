7c1986f55230f30383ebf3277327928549796407
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2025-12-24 04:50:31 +0900

    패킷<->byte 변환용 래퍼 작성

---
+++ Shared/Network/IPacketByteConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Network
+{
+    public interface IPacketByteConverter
+    {
+        public void Read(ArraySegment<byte> segment);
+        public ArraySegment<byte> Write();
+    }
+}
