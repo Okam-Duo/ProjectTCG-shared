797cdff34dffaaf9af137b1ec47763738eb7c7be
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2025-12-31 05:20:31 +0900

    선택 가능한 대상을 나타내는 데이터 작성

---
+++ Shared/Contents/SkillTargetFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Contents
+{
+    public struct SkillTargetFilter
+    {
+        public int choiceCount;
+        public TargetingType targetingType;
+        public TargetTeam team;
+
+        //어떤 팀의 대상을 타겟으로 정할 수 있는지
+        public enum TargetTeam
+        {
+            None,
+            Ally,
+            Enemy,
+            All,
+        }
+
+        //영웅 선택인지, 특정 팀의 영웅 전체 선택인지
+        public enum TargetingType
+        {
+            None,
+            Hero,
+            Team,
+        }
+    }
+}
