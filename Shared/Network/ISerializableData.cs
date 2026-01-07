10aab7aba564d45d6a0948983d6cef14cd5dfb82
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2026-01-08 07:59:51 +0900

    직렬화 가능한 데이터 인터페이스 작성

---
+++ Shared/Network/ISerializableData.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Network
+{
+    public interface ISerializableData
+    {
+        public void Read(ReadOnlySpan<byte> segment, ref int count);
+        public bool Write(Span<byte> segment, ref int count);
+    }
+}
